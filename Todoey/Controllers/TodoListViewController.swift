b8a8e2140abfe38fb0ec4c1c51cbda28e6601a1e
Burak Emre Toker <burakemretoker@gmail.com>
2023-08-09 16:30:19 +0300

    Create CoreData

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,31 +10,34 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemsArray = [Item]()
-    var defaults = UserDefaults.standard
-
+    //    var defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathExtension("Items.plist")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("The Data File Path: \(dataFilePath!)")
         
         let newItem1 = Item()
         newItem1.title = "Find Mike"
         itemsArray.append(newItem1)
         
         let newItem2 = Item()
-        newItem2.title = "Buy Eggs"
-        newItem2.done = true
+        newItem2.title = "Buy Eggos"
         itemsArray.append(newItem2)
         
         let newItem3 = Item()
         newItem3.title = "Destroy Demogorgon"
         itemsArray.append(newItem3)
         
-  
+        loadItems()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemsArray = items
-        }
+        //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+        //            itemsArray = items
+        //        }
         
+        // write the code that above' NSCoder version (above was NSUserDefault).
         
     }
     
@@ -49,22 +52,20 @@ class TodoListViewController: UITableViewController {
         
         let item = itemsArray[indexPath.row]
         
-
+        
         cell.textLabel?.text = item.title
         
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
+        //        if item.done == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //        }
         
         // There is even further easy implementation of this as "Ternary":
         cell.accessoryType = item.done == true ? .checkmark : .none
-
+        
         
         return cell
-        
-        
         
     }
     
@@ -72,19 +73,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
-//        if itemsArray[indexPath.row].done == false {
-//            itemsArray[indexPath.row].done = true
-//        } else {
-//            itemsArray[indexPath.row].done = false
-//        }
+        //        if itemsArray[indexPath.row].done == false {
+        //            itemsArray[indexPath.row].done = true
+        //        } else {
+        //            itemsArray[indexPath.row].done = false
+        //        }
         
         // instead :
         itemsArray[indexPath.row].done = !itemsArray[indexPath.row].done
         
+        // We call it whenever we want to save file, also in here.
+        self.saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         tableView.reloadData()
-        
         
     }
     
@@ -97,12 +99,16 @@ class TodoListViewController: UITableViewController {
             // What will happen when user clicks the action button.
             print(textField.text!)
             
-            self.defaults.set(self.itemsArray, forKey: "TodoListArray")
+            //            self.defaults.set(self.itemsArray, forKey: "TodoListArray")
             
             // Adding data to our system and reloading data.
             let newItem = Item()
             newItem.title = textField.text!
             self.itemsArray.append(newItem)
+            
+            // We call it whenever to save file.
+            self.saveItems()
+            
             self.tableView.reloadData()
         }
         
@@ -115,8 +121,29 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true)
     }
     
-
-
-
+    // This is the method to we able to save our data into the .plist file.
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemsArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding itemArray: \(error)")
+        }
+    }
+    
+    func loadItems() {
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let data = try Data(contentsOf: dataFilePath!)
+            itemsArray = try decoder.decode([Item].self, from: data)
+        } catch {
+            print("Error decodind itemArray: \(error)")
+        }
+    }
+    
+    
 }
 
--- Todoey/Model/Item.swift
+++ Todoey/Model/Item.swift
@@ -5,9 +5,13 @@
 //  Created by Burak Emre Toker on 8.08.2023.
 //
 
-import Foundation
+//import Foundation
+//
+//
+//// To be encodable, should include standart dataType!
+//class Item: Codable {
+//    var title: String = ""
+//    var done: Bool = false
+//}
 
-class Item {
-    var title: String = ""
-    var done: Bool = false
-}
+// We don't need that anymore.
